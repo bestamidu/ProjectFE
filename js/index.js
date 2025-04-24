22b097ea15ff00bb56d49ef1c3469d05b19ce4f1
bestamidu <ynhu22887@gmail.com>
2025-04-24 10:02:30 +0700

    task1

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("accountModal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("accountModal").style.display = "none";
+  }
